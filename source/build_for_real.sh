0c974f92578e69e35b7f88ba216a2d81199e4171
Gordan Zitkovic <gordan.zitkovic@gmail.com>
2024-01-23 12:10:03 -0600

    No_message

--- source/build_for_real.sh
+++ source/build_for_real.sh
@@ -2,6 +2,6 @@
 
 set -ev
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', output_dir='../doc')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', output_dir='../docs')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
